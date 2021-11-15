1611867bc08137d7c4b4a0d4212925c31f13bded
ksyasuda <ksyasuda@umich.edu>
2021-11-14 23:54:19 -0800

    add laptop config and fix stuff

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -9,4 +9,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # polybar -c ~/i3/configs/config.ini main &
-polybar -c ~/SudacodeRice/polybar/config.ini main &
+# polybar -c ~/SudacodeRice/polybar/config.ini main &
+# polybar -c ~/SudacodeRice/polybar/config.ini main &
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		# MONITOR=$m polybar --reload -c ~/SudacodeRice/polybar/config.desktop.ini main &
+		# update to use new theme
+		MONITOR=$m polybar --reload -c "$HOME/SudacodeRice/polybar-themes/sblocks/config-4k.ini" main &
+	done
+else
+	polybar --reload example &
+fi
