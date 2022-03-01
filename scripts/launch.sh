4307fa28dbd4deca41fc71f55702d53701971ea4
ksyasuda <ksyasuda@umich.edu>
2022-03-01 00:20:28 -0800

    updates

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,18 +6,18 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$UID" -x polybar > /dev/null; do sleep 1; done
 
 # polybar -c ~/i3/configs/config.ini main &
 # polybar -c ~/SudacodeRice/polybar/config.ini main &
 # polybar -c ~/SudacodeRice/polybar/config.ini main &
 
 if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		# MONITOR=$m polybar --reload -c ~/SudacodeRice/polybar/config.desktop.ini main &
-		# update to use new theme
-		MONITOR=$m polybar --reload -c "$HOME/SudacodeRice/polybar-themes/sblocks/config-4k.ini" main &
-	done
+    for m in $(xrandr --query | grep -E " connected" | cut -d" " -f1); do
+        # MONITOR=$m polybar --reload -c ~/SudacodeRice/polybar/config.desktop.ini main &
+        # update to use new theme
+        MONITOR=$m polybar --reload -c "$HOME/SudacodeRice/polybar-themes/sblocks/config-4k.ini" main &
+    done
 else
-	polybar --reload example &
+    polybar --reload example &
 fi
