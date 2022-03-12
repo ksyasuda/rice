3dfed596cb91c8ce7debe18d11b4d3bb51084edf
ksyasuda <ksyasuda@umich.edu>
2022-03-11 19:46:47 -0800

    update doom config

--- doom/config.el
+++ doom/config.el
@@ -5,36 +5,60 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Kyle Yasuda"
-      user-mail-address "ksyasuda@umich.edu")
+      user-mail-address "the.sudacode@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-;; (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14))
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 12.0))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
 
-;; Here are some additional functions/macros that could help you configure Doom:
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -47,86 +71,34 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+;;-------;;
+;; VTERM ;;
+;;-------;;
+(after! vterm
+  (set-popup-rule! "*doom:vterm-popup:main" :size 0.45 :vslot -4 :select t :quit nil :ttl 0 :side 'right)
+)
 
-
-(setq python-prettify-symbols-alist
-  '(("lambda"  . ?λ)
-    ("and" . ?∧)
-    ("or" . ?∨)
-    ("in" . ?∈)
-    ("for" . ?∀)
-    ("def" . ?ƒ)
-    ("int" . ?ℤ)
-    ("not" . ?¬)))
-
-;; (setq prettify-symbols-alist
-;;   '(("lambda"  . ?λ)
-;;     ("&&" . ?∧)
-;;     ("||" . ?∨)
-;;     ("in" . ?∈)
-;;     ("for" . ?∀)
-;;     ("function" . ?ƒ)
-;;     ("int" . ?ℤ)
-;;     ("not" . ?¬)))
-
-
+;;----------;;
+;; WAKATIME ;;
+;;----------;;
 (use-package! wakatime-mode :ensure t)
 (global-wakatime-mode)
 
+;;-----------;;
+;; FIRA CODE ;;
+;;-----------;;
+;; (use-package! fira-code-mode
+;;   :hook prog-mode)
 
-(use-package! fira-code-mode
-  :hook prog-mode)
-
-;;; LSP
-(use-package! lsp
-  :init
-  (setq lsp-pyls-plugins-pylint-enabled t)
-  (setq lsp-pyls-plugins-autopep8-enabled t)
-  (setq lsp-pyls-plugins-yapf-enabled t)
-  (setq lsp-pyls-plugins-pyflakes-enabled t)
-)
-
-(lsp-ui-mode)
-
-;; (require 'lsp-python-ms)
-;; (use-package lsp-python-ms
-;;   :init (setq lsp-python-ms-auto-install-server t)
-;;   :hook (python-mode . (lambda ()
-;;                           (require 'lsp-python-ms)
-;;                           (lsp))))  ; or lsp-deferred
-
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp))))  ; or lsp-deferred
-
-(use-package! lsp-mode
-  :commands lsp
-  :hook
-  (sh-mode . 'lsp))
-
-(setq lsp-enable-folding t)
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)))
-
-
-;; (setq python-shell-interpreter "ipython"
-;;         python-shell-interpreter-args "-i --simple-prompt --InteractiveShell.display_page=True")
-
-; (setf (lsp-session-folders-blacklist (lsp-session)) nil)
-; (lsp--persist-session (lsp-session))
-
-(advice-add 'lsp :before (lambda (&rest _args) (eval '(setf (lsp-session-server-id->folders (lsp-session)) (ht)))))
-
-;;; all the icons
-
+;;---------------;;
+;; ALL THE ICONS ;;
+;;---------------;;
 (add-load-path! (expand-file-name "~/Downloads/all-the-icons-dired"))
 (load "all-the-icons-dired.el")
 (use-package! all-the-icons-dired
@@ -147,104 +119,50 @@
     (all-the-icons-dired-mode 1)))
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 
-
-;;; enacs application framework
-(use-package eaf
-  :load-path "~/Downloads/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-  :custom
-  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
-  (eaf-browser-continue-where-left-off t)
-  (eaf-browser-enable-adblocker t)
-  (browse-url-browser-function 'eaf-open-browser)
-  :config
-  (defalias 'browse-web #'eaf-open-browser))
-
-(require 'eaf-system-monitor)
-(require 'eaf-pdf-viewer)
-(require 'eaf-terminal)
-(require 'eaf-music-player)
-(require 'eaf-browser)
-(require 'eaf-jupyter)
-(require 'eaf-file-browser)
-(require 'eaf-markdown-previewer)
-(require 'eaf-image-viewer)
-(require 'eaf-file-manager)
-(require 'eaf-airshare)
-(require 'eaf-org-previewer)
-(require 'eaf-file-sender)
-(require 'eaf-video-player)
-
-(require 'eaf-evil)
-
-
-(define-key key-translation-map (kbd "SPC")
-    (lambda (prompt)
-      (if (derived-mode-p 'eaf-mode)
-          (pcase eaf--buffer-app-name
-            ("browser" (if  (string= (eaf-call-sync "call_function" eaf--buffer-id "is_focus") "True")
-                           (kbd "SPC")
-                         (kbd eaf-evil-leader-key)))
-            ("pdf-viewer" (kbd eaf-evil-leader-key))
-            ("image-viewer" (kbd eaf-evil-leader-key))
-            (_  (kbd "SPC")))
-        (kbd "SPC"))))
-
-
-;;; vterm
-
-(after! vterm
-  (set-popup-rule! "*doom:vterm-popup:main" :size 0.45 :vslot -4 :select t :quit nil :ttl 0 :side 'right)
-)
-
-;;; gif screencast
-
-(with-eval-after-load 'gif-screencast
-  (define-key gif-screencast-mode-map (kbd "<f8>") 'gif-screencast-toggle-pause))
-
-(global-set-key (kbd "<f9>") 'gif-screencast-start-or-stop)
-
-
-;;; HOOKS
-
-
-(add-hook 'org-mode-hook #'org-bullets-mode)
-(add-hook 'before-save-hook 'py-isort-before-save)
-
-(after! flyspell
-  (setq flyspell-lazy-idle-seconds 2))
-
-;;; VARIABLES
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+;;-----------;;
+;; VARIABLES ;;
+;;-----------;;
+;; :set relativenumber
+(setq display-line-numbers-type 'relative)
 (setq rainbow-delimiters-mode t)
 (setq confirm-kill-emacs nil)
+(setq browse-url-browser-function 'browse-url-firefox)
+(setq prettify-symbols-mode nil)
+(setq +pretty-code-enabled-modes nil)
 (setq prettify-symbols-mode nil)
 (setq global-prettify-symbols-mode nil)
-(setq browse-url-browser-function 'browse-url-firefox)
+(setq lsp-diagnostic-package :none)
+(setq shfmt-arguments '("-i" "0" "-ci" "-sr"))
 
+;; (setq lsp-ui-doc-position 'bottom)
+;; (setq lsp-ui-doc-alignment 'window)
+;; (setq lsp-ui-doc-max-height 25)
+;; (setq lsp-ui-doc-max-width 350)
+;; (setq lsp-ui-doc-mode t)
+;; (setq lsp-ui-peek-mode t)
+;; (setq lsp-ui-peek-enable t)
+;; (setq lsp-ui-doc-delay 0.25)
 
-(setq lsp-ui-doc-position 'bottom)
-(setq lsp-ui-doc-alignment 'window)
-(setq lsp-ui-doc-max-height 25)
-(setq lsp-ui-doc-max-width 350)
-(setq lsp-ui-doc-mode t)
-(setq lsp-ui-peek-mode t)
-(setq lsp-ui-peek-enable t)
-(setq lsp-ui-doc-delay 0.25)
+;; (setq eaf-terminal-font-size 12)
+;; (setq lsp-treemacs-sync-mode 1)
 
-(setq eaf-terminal-font-size 12)
-(setq lsp-treemacs-sync-mode 1)
-
-;; (setq lsp-python-ms-auto-install-server t)
+;;--------;;
+;; HOOKS  ;;
+;;--------;;
+(add-hook 'org-mode-hook #'org-bullets-mode)
+(add-hook 'before-save-hook 'py-isort-before-save)
 (add-hook 'python-mode-hook #'lsp) ; or lsp-deferred
+(add-hook 'sh-mode-hook 'shfmt-on-save-mode 'flycheck-mode)
 
-
+;;--------;;
+;; AFTER  ;;
+;;--------;;
 (after! 'treemacs
   (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action))
 
-;;; KEYBINDINGS
-
+;;--------------;;
+;; KEYBINDINGS  ;;
+;;--------------;;
 (map! :leader
       (:prefix ("o" . "+open")
       :desc "Launch lsp-ui-imenu"
